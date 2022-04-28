9d219f4b15b9fabf1670aa3d9dd934290eb0abd5
ykllog <info@ykllog.com>
2022-04-28 14:39:12 +0900

    複数検索時の不具合修正

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
-function getValue(){
-  let ncodeValue = ncode.value.replace('　',' ');
-  let wordValue = word.value.replace('　',' ');
-  let negativeValue = negative.value.replace(' ', ' -').replace('　', ' -');
+$(function () {
+  $('#submit').on('click', function() {
+    const ncodeArray = ncode.value.replace(/　/g,' ').replace(/,/g,' ').replace(/，/g,' ').replace(/、/g,' ');
+    const ncodeValue = ncodeArray.split(' ');
+    let ncodeData = [];
+    for (i = 0; i < ncodeValue.length; i++) {
+      ncodeData += ncodeValue[i] + ' OR ';
+    }
+    let ncodeInput = ncodeData.slice(0, -4);
 
-  if (Object.keys(ncodeValue).length && Object.keys(negativeValue).length) {
-    let searchValue = `${ncodeValue} ${wordValue} -${negativeValue}`;
-    searchWord.value = searchValue;
-  } else if (Object.keys(ncodeValue).length && !Object.keys(negativeValue).length) {
-    let searchValue = `${ncodeValue} ${wordValue}`;
-    searchWord.value = searchValue;
-  } else if (!Object.keys(ncodeValue).length && Object.keys(negativeValue).length) {
-    let searchValue = `${wordValue} -${negativeValue}`;
-    searchWord.value = searchValue;
-  } else {
-    let searchValue = `${wordValue}`;
-    searchWord.value = searchValue;
-  }
-}
+    const wordArray = word.value.replace(/　/g,' ').replace(/,/g,' ').replace(/，/g,' ').replace(/、/g,' ');
+    const wordValue = wordArray.split(' ');
+    let wordData = [];
+    for (i = 0; i < wordValue.length; i++) {
+      wordData += wordValue[i] + ' ';
+    }
+    let wordInput = wordData.slice(0, -1);
+
+    const negativeArray = negative.value.replace(/　/g, ' ').replace(/ /g,' ').replace(/,/g,' ').replace(/，/g,' ').replace(/、/g,' ');
+    const negativeValue = negativeArray.split(' ');
+    let negativeData = [];
+    for (i = 0; i < negativeValue.length; i++) {
+      negativeData += '-' + negativeValue[i] + ' ';
+    }
+    let negativeInput = negativeData.slice(0, -1);
+
+    if (Object.keys(ncodeArray).length && Object.keys(negativeArray).length) {
+      let searchValue = ncodeInput + ' ' + wordInput +  ' ' + negativeInput;
+      searchWord.value = searchValue;
+    } else if (Object.keys(ncodeArray).length && !Object.keys(negativeArray).length) {
+      let searchValue = ncodeInput + ' ' + wordInput;
+      searchWord.value = searchValue;
+    } else if (!Object.keys(ncodeArray).length && Object.keys(negativeArray).length) {
+      let searchValue = wordInput +  ' ' + negativeInput;
+      searchWord.value = searchValue;
+    } else {
+      let searchValue = wordInput;
+      searchWord.value = searchValue;
+    }
+
+  });
+});
