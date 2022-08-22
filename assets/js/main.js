62e9dcb320376fad44bc68db6026f9ac0e7c4144
ykllog <info@ykllog.com>
2022-08-22 19:56:23 +0900

    indexページ修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
+//*------------------*
+//*  hamburger
+//*------------------*
+
 $(function () {
-  $('#submit').on('click', function() {
-    const ncodeArray = ncode.value.replace(/　/g,' ').replace(/,/g,' ').replace(/，/g,' ').replace(/、/g,' ');
+  $('.header_hamburger').on('click', function () {
+    $('.header_hamburger, .header_nav').toggleClass('open');
+    $('body').toggleClass('noScroll');
+  });
+});
+
+//*------------------*
+//*  ncodeForm
+//*------------------*
+
+$(function () {
+  $('#submit').on('click', function () {
+    const ncodeArray = ncode.value.replace(/　/g, ' ').replace(/,/g, ' ').replace(/，/g, ' ').replace(/、/g, ' ');
     const ncodeValue = ncodeArray.split(' ');
     let ncodeData = [];
     for (i = 0; i < ncodeValue.length; i++) {
@@ -8,7 +23,7 @@ $(function () {
     }
     let ncodeInput = ncodeData.slice(0, -4);
 
-    const wordArray = word.value.replace(/　/g,' ').replace(/,/g,' ').replace(/，/g,' ').replace(/、/g,' ');
+    const wordArray = word.value.replace(/　/g, ' ').replace(/,/g, ' ').replace(/，/g, ' ').replace(/、/g, ' ');
     const wordValue = wordArray.split(' ');
     let wordData = [];
     for (i = 0; i < wordValue.length; i++) {
@@ -16,7 +31,7 @@ $(function () {
     }
     let wordInput = wordData.slice(0, -1);
 
-    const negativeArray = negative.value.replace(/　/g, ' ').replace(/ /g,' ').replace(/,/g,' ').replace(/，/g,' ').replace(/、/g,' ');
+    const negativeArray = negative.value.replace(/　/g, ' ').replace(/ /g, ' ').replace(/,/g, ' ').replace(/，/g, ' ').replace(/、/g, ' ');
     const negativeValue = negativeArray.split(' ');
     let negativeData = [];
     for (i = 0; i < negativeValue.length; i++) {
@@ -25,13 +40,13 @@ $(function () {
     let negativeInput = negativeData.slice(0, -1);
 
     if (Object.keys(ncodeArray).length && Object.keys(negativeArray).length) {
-      let searchValue = ncodeInput + ' ' + wordInput +  ' ' + negativeInput;
+      let searchValue = ncodeInput + ' ' + wordInput + ' ' + negativeInput;
       searchWord.value = searchValue;
     } else if (Object.keys(ncodeArray).length && !Object.keys(negativeArray).length) {
       let searchValue = ncodeInput + ' ' + wordInput;
       searchWord.value = searchValue;
     } else if (!Object.keys(ncodeArray).length && Object.keys(negativeArray).length) {
-      let searchValue = wordInput +  ' ' + negativeInput;
+      let searchValue = wordInput + ' ' + negativeInput;
       searchWord.value = searchValue;
     } else {
       let searchValue = wordInput;
